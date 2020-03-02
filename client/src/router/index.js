abb6ba4df9d90b24d32978c21ad718c8bb102105
Henrik Holm <henholm@kth.se>
2020-03-02 21:28:18 +0100

    Added authentication step in router

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -4,11 +4,15 @@ import TimeSlotsView from '../views/TimeSlots.vue';
 import BookTimeSlotView from '../views/BookTimeSlot.vue';
 import AssistantLoginView from '../views/AssistantLogin.vue';
 import AssistantAdminView from '../views/AssistantAdmin.vue';
+import LoginView from '../views/Login.vue';
+import store from '../store';
 
 Vue.use(VueRouter);
 
 const routes = [
-  { path: '/', redirect: '/timeSlots' },
+  { path: '/', redirect: '/Login' },
+  // { path: '/', redirect: '/timeSlots' },
+  { path: '/Login', component: LoginView },
   { path: '/timeSlots', component: TimeSlotsView },
   { path: '/bookTimeSlot/:timeSlotId', component: BookTimeSlotView },
   { path: '/assistantLogin', component: AssistantLoginView },
@@ -21,6 +25,16 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
+});
+
+// Setup Authentication guard
+router.beforeEach((to, from, next) => {
+  if (store.state.isAuthenticated || to.path === '/login') {
+    next();
+  } else {
+    console.info('Unauthenticated user. Redirecting to login page.');
+    next('/login');
+  }
 });
 
 export default router;
