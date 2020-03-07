103d919455468326b879f4d08300fc7274e29064
Henrik Holm <henholm@kth.se>
2020-03-07 04:51:11 +0100

    Added isLoggedIn attribute

--- server/src/instantiate_database.js
+++ server/src/instantiate_database.js
@@ -41,12 +41,12 @@ TimeSlot.belongsTo(Assistant, { foreignKey: 'assistantId' });
   console.log('All models were synchronized successfully.');
 
   // Populate tables. The create method combines build() and save().
-  await User.create({ name: 'assistant1', password: 'password1', isAssistant: 1 });
-  await User.create({ name: 'assistant2', password: 'password2', isAssistant: 1 });
-  await User.create({ name: 'assistant3', password: 'password3', isAssistant: 1 });
-  await User.create({ name: 'student1', password: 'password1', isAssistant: 0 });
-  await User.create({ name: 'student2', password: 'password2', isAssistant: 0 });
-  await User.create({ name: 'student3', password: 'password3', isAssistant: 0 });
+  await User.create({ name: 'assistant1', password: 'password1', isAssistant: 1, isLoggedIn: 0 });
+  await User.create({ name: 'assistant2', password: 'password2', isAssistant: 1, isLoggedIn: 0 });
+  await User.create({ name: 'assistant3', password: 'password3', isAssistant: 1, isLoggedIn: 0 });
+  await User.create({ name: 'student1', password: 'password1', isAssistant: 0, isLoggedIn: 0 });
+  await User.create({ name: 'student2', password: 'password2', isAssistant: 0, isLoggedIn: 0 });
+  await User.create({ name: 'student3', password: 'password3', isAssistant: 0, isLoggedIn: 0 });
 
   await Assistant.create({ id: 1, name: 'assistant1' });
   await Assistant.create({ id: 2, name: 'assistant2' });
