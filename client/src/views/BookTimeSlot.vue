c7c338b61fd13a812b6877505561127c0551825e
Henrik Holm <henholm@kth.se>
2020-03-08 19:40:49 +0100

    Fixed logout bug

--- client/src/views/BookTimeSlot.vue
+++ client/src/views/BookTimeSlot.vue
@@ -1,6 +1,6 @@
 
 <template>
-  <div class="text-box col-md-4 col-md-offset-4" style="text-align: center">
+  <div v-if="this.show" class="text-box col-md-4 col-md-offset-4" style="text-align: center">
     <h1>Time slot booking</h1>
     <div>
       <h4>Time: {{this.timeSlotTime}}</h4>
@@ -26,6 +26,7 @@ export default {
   components: {},
   data() {
     return {
+      show: false,
       countdownTime: '',
       countdownSeconds: 20,
       timeSlotId: this.$route.params.timeSlotId,
@@ -59,57 +60,29 @@ export default {
       }, 1000);
     },
   },
-  beforeRouteEnter(to, from, next) {
-    console.log(`beforeRouteEnter: routing from ${from.path} to ${to.path}`);
-    const payload = {
-      timeSlotId: to.params.timeSlotId,
-    };
-    // Check if the requested time slot exists. If not, redirect backwards.
-    RoutingService.getTimeSlotData(payload).then((response) => {
-      console.log(response);
-      /* eslint-disable no-param-reassign */
-      next((vm) => {
-        console.log(vm);
+  // Step 2 in lifecycle hooks.
+  created() {
+    this.socket = this.$root.socket;
+    if (!this.$store.getters.isLoggedIn) {
+      this.$router.push('/login');
+    } else {
+      const payload = {
+        timeSlotId: this.timeSlotId,
+      };
+      RoutingService.getTimeSlotData(payload).then((response) => {
+        this.socket.emit('changeState', { id: this.timeSlotId, bookedBy: 'reserved' });
+        this.countdown();
         const { timeSlotData } = response;
-        vm.timeSlotId = timeSlotData.id;
-        vm.timeSlotTime = timeSlotData.time;
-        vm.assistantId = timeSlotData.assistantId;
-        vm.assistantName = timeSlotData.assistantName;
-        next();
+        this.timeSlotId = timeSlotData.id;
+        this.timeSlotTime = timeSlotData.time;
+        this.assistantId = timeSlotData.assistantId;
+        this.assistantName = timeSlotData.assistantName;
+        this.show = true;
+      }).catch((err) => {
+        console.log(err);
+        this.$router.go(-1);
       });
-      /* eslint-enable no-param-reassign */
-    }).catch((err) => {
-      console.log(err);
-      // this.$router.go(-1);
-      // return next({ path: from.path });
-      return next(from);
-    });
+    }
   },
-  // Step 1 in lifecycle hooks.
-  // beforeCreate() {
-  //   this.socket = this.$root.socket;
-  //   console.log(this.socket);
-  //   console.log(this.$store.getters.isLoggedIn);
-  //   if (!this.$store.getters.isLoggedIn) {
-  //     this.$router.push('/login');
-  //   } else {
-  //     const payload = {
-  //       timeSlotId: this.timeSlotId,
-  //     };
-  //     console.log(payload);
-  //     RoutingService.getTimeSlotData(payload).then((response) => {
-  //       this.socket.emit('changeState', { id: this.timeSlotId, bookedBy: 'reserved' });
-  //       this.countdown();
-  //       const { timeSlotData } = response;
-  //       this.timeSlotId = timeSlotData.id;
-  //       this.timeSlotTime = timeSlotData.time;
-  //       this.assistantId = timeSlotData.assistantId;
-  //       this.assistantName = timeSlotData.assistantName;
-  //     }).catch((err) => {
-  //       this.$router.go(-1);
-  //       console.log(err);
-  //     });
-  //   }
-  // },
 };
 </script>
